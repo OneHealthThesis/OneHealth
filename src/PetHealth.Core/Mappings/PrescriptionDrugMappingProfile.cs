9f434b22c57da2e4ae4073a7fc6cf3b4681352e6
Victor Lantigua <v.lantigua94@gmail.com>
2022-11-07 12:48:18 -0500

    Add new mapping profiles.

---
+++ src/PetHealth.Core/Mappings/PrescriptionDrugMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using PetHealth.Core.DTOs.EntityDTO;
+using PetHealth.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHealth.Core.Mappings
+{
+    public class PrescriptionDrugMappingProfile: Profile
+    {
+        public PrescriptionDrugMappingProfile()
+        {
+            CreateMap<PrescriptionDrugDTO, PrescriptionDrug>()
+                .ForMember(src => src.Id, opt => opt.MapFrom(opt => opt.Id))
+                .ForMember(src => src.PersonId, opt => opt.MapFrom(opt => opt.PersonId))
+                .ForMember(src => src.PetId, opt => opt.MapFrom(opt => opt.PetId))
+                .ForMember(src => src.Date, opt => opt.MapFrom(opt => opt.Date))
+                .ForMember(src => src.Place, opt => opt.MapFrom(opt => opt.Place))
+                .ForMember(src => src.Doctor, opt => opt.MapFrom(opt => opt.Doctor))
+                .ForMember(src => src.Notes, opt => opt.MapFrom(opt => opt.Notes))
+                .ForMember(src => src.CreatedOnDBDate, opt => opt.Ignore())
+                .ReverseMap();
+        }
+    }
+}
---
+++ src/PetHealth.Core/Mappings/VaccinesMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using PetHealth.Core.DTOs;
+using PetHealth.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetHealth.Core.Mappings
+{
+    public class VaccinesMappingProfile: Profile
+    {
+        public VaccinesMappingProfile()
+        {
+            CreateMap<LongIdNameDTO, Vaccines>()
+               .ForMember(src => src.Id, opt => opt.MapFrom(dto => dto.Id))
+               .ForMember(src => src.Name, opt => opt.MapFrom(dto => dto.Name))
+               .ForMember(src => src.CreatedOnDBDate, opt => opt.Ignore())
+               .ReverseMap();
+        }
+    }
+}
